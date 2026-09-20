cfef08f4cee552a505bd734e000a127e1d6c3256
lexs32 <hadjmed2030@gmail.com>
2026-09-21 00:38:07 +0100

    Restore ChamsCheats GSAP entrance and scroll animations across all pages

--- deploy.bat
+++ deploy.bat
@@ -6,6 +6,6 @@ if not exist ".git" (
     git remote add origin https://github.com/lexs32/hiddencheats.git
 )
 git add .
-git commit -m "Deploy HiddenCheats storefront to production"
+git commit -m "Restore ChamsCheats GSAP entrance and scroll animations across all pages"
 git push -u origin master
 npx vercel --prod
